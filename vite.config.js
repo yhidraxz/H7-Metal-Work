406b3d4b8825640399b8252af6839a5802b2c419
hidra <yhidraxz@gmail.com>
2025-07-18 20:42:48 -0300

    fixed syntax error on github deploy url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/H7-Metal-Work",
+  base: "/H7-Metal-Work/",
   plugins: [react()],
 });
